c7fd848384bd14237b478326307ae5e794a283c0
karoljaw <karoljaworek04@gmail.com>
2018-01-03 10:12:37 +0100

    Add files via upload

--- form.php
+++ form.php
@@ -3,7 +3,7 @@ $password = $_GET["password"];
 $email = $_GET["email"];
 $location = $_GET["location"];
 
-$mailmassage = "Masz wiadomość od ".first " from ".$location;
+$mailmassage = "Masz wiadomość od ".$first " from ".$location;
 
 $to = karoljaworek04@gmail.com;
 $subject = "My website form";
